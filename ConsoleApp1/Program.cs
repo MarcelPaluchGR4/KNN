b2c8d51e3d5fe4e124270331731ad7d6a8b9e556
MarcelPaluchGR4 <parcelmurzyn@gmail.com>
2025-06-07 18:34:51 +0200

    WIP

--- ConsoleApp1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿string[] lines = File.ReadAllLines("training_data.csv");
-
-foreach (string line in lines)
-{
-    string[] parts = line.Split(',');
-    if (parts.Length < 5) continue;
-}
---
+++ zad1/Program.cs
@@ -0,0 +1,29 @@
+﻿class KNN
+{
+    public string DistanceType;
+
+    public double CalculateDistance(double[] a, double[] b)
+    {
+        double sum = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (DistanceType == "euclidean")
+            {
+                sum += (a[i] - b[i]) * (a[i] - b[i]);
+            }
+            else if (DistanceType == "manhattan")
+            {
+                sum += Math.Abs(a[i] - b[i]);
+            }
+        }
+
+        if (DistanceType == "euclidean")
+        {
+            return Math.Sqrt(sum);
+        }
+        else
+        {
+            return sum;
+        }
+    }
+}
